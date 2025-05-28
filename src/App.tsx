873861fd87a9754bea5618b086ab425fcfe4c1ed
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-05-28 08:12:05 +0700

    make route in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Header from "./component/Header";
-import Hero from "./component/Hero";
-import About from "./component/About";
-import VisiMisi from "./component/VisiMisi";
-import Portfolio from "./component/Portofolio";
-import MileStone from "./component/MileStone";
-import Contact from "./component/Contact";
-import Footer from "./component/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Blog from "./pages/Blog";
 import ScrollTop from "./component/ScrollTop";
-import InstagramPOST from "./component/PostinganIG";
 
-const App = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // durasi animasi dalam ms
-      once: true, // animasi hanya terjadi sekali
-      offset: 100, // offset dari trigger point
-    });
-  }, []);
-
+function App() {
   return (
-    <>
-      <Header></Header>
-      <main className="main">
-        <Hero />
-        <About />
-        <VisiMisi />
-        <MileStone />
-        <Portfolio />
-        <InstagramPOST />
-        <Contact />
-      </main>
-      <Footer />
+    <Router>
       <ScrollTop />
-    </>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
